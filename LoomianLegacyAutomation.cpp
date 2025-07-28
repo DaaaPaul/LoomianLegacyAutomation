cc19f0aa01e5f020224c60f7e68811e7d3558da2
DaaaPaul <paulpeng2008@gmail.com>
2025-07-28 12:16:17 -0400

    Get mouse move (something) working

--- LoomianLegacyAutomation.cpp
+++ LoomianLegacyAutomation.cpp
@@ -1,7 +1,10 @@
 ﻿#include <iostream>
+#include "externals/robot-cpp/src/Mouse.h"
 
 using namespace std;
 
 int main() {
 	cout << "Hello CMake." << endl;
+
+	Robot::Mouse::MoveSmooth({ 100, 200 });
 }
